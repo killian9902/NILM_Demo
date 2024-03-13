994b46bea83dab60b67fbee62f3a221528271f7f
killian9902 <killiandempsey.tech@gmail.com>
2024-03-13 15:21:12 +0400

    Models/DB Added

--- training/models.py
+++ training/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Appliance(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class MeterReading(models.Model):
+    appliance = models.ForeignKey(Appliance, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    Wh = models.FloatField()
+
+    def __str__(self):
+        return f'{self.appliance.name} - {self.datetime}: {self.Wh}Wh'
